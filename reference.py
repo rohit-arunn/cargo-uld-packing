af10ad93fc4fc9a80164374a4236edbdb1b377f6
rohit-arunn <133693778+rohit-arunn@users.noreply.github.com>
2025-09-13 18:25:13 +0530

    Update reference.py

--- reference.py
+++ reference.py
@@ -1,12 +1,23 @@
 import pandas as pd
+import math
 
-# Load your dataset
-df = pd.read_parquet("LD1_items.parquet")  # or use read_csv(...) if it's CSV
+df = pd.read_parquet("LD1_items.parquet") 
 
-# Filter for the desired flight
-flight_df = df[(df['dim_vol'] < 0.006)]
+df['pcslen'] = df['pcslen'].apply(math.ceil)
+df['pcshgt'] = df['pcshgt'].apply(math.ceil)
+df['pcswid'] = df['pcswid'].apply(math.ceil) 
 
-# Save to CSV
-flight_df.to_parquet("flight_ICN_to_BUD.parquet", index=False)
+
+
+flight_df = df[(df['dim_vol'] < 0.16) & (df['dim_numpcs'] == 3 )]      
+flight_df['volume'] = flight_df['pcslen']*flight_df['pcshgt']*flight_df['pcswid'] 
+
+df_sorted = flight_df.sort_values(by='volume', ascending=False) 
+df_sorted = df_sorted[(df_sorted['volume'] < 5000)]
+
+
+df_sorted.to_parquet("flight_ICN_to_BUD.parquet", index=False)
 
 print("Saved flight data to 'flight_ICN_to_BUD.csv'")
+#print(df_sorted['pcslen']) 
+
